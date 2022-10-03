fc8c046f6604fbf7de658afc85112dd6a3dd48f1
dorteks <ayodelealayemi@gmail.com>
2022-10-03 15:35:17 +0100

    removed seed data from prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,10 @@
-import { User } from "./user";
-import { Address } from "./address";
-import { Products } from "./products";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log(`Start seeding.....`);
-  for (const user of User) {
-    await prisma.user.create({
-      data: user,
-    });
-  }
-
-  for (const address of Address) {
-    await prisma.address.create({
-      data: address,
-    });
-  }
-
-  for (const product of Products) {
-    await prisma.product.create({
-      data: product,
-    });
-  }
+  //seeding
   console.log(`Finsished seeding.`);
 }
 
